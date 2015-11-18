e1f4e11c89cd2578157e517557bb17d656a56e13
TL Deng <2942182050@qq.com>
2015-11-18 13:01:42 +0800

    add decryption algorithm

--- columnPermutation.c
+++ columnPermutation.c
@@ -9,11 +9,14 @@ int times[26];
 int pos[26];
 int permutation[128];
 int col;
+int row_num = 0;
 int count = 0;
 int col_length_count[128];
+int order[128];
 
 FILE* plaintext;
 FILE* ciphertext;
+FILE* result;
 
 void get_permutation() {
 	int len = strlen(key);
@@ -45,6 +48,8 @@ void get_permutation() {
 	printf("\n");
 }
 
+
+
 void encrypt() {
 	int i;
 	// create strings to store each column's data.
@@ -67,7 +72,7 @@ void encrypt() {
 	while ( !feof(plaintext) ) {
 		col_num = count % col;
 		index = col_length_count[col_num];
-		printf("col_num = %d index = %d data = %c\n", col_num, index, c);
+		//printf("col_num = %d index = %d data = %c\n", col_num, index, c);
 		cols[col_num][index] = c;
 		col_length_count[col_num]++;
 		count++;
@@ -79,10 +84,10 @@ void encrypt() {
 	}
 	
 	// fill 'q' at shorter column
-	int row_num = strlen(cols[0]);
+	row_num = strlen(cols[0]);
 	for (i = 1; i < col; i++) {
 		if (strlen(cols[i]) < row_num) {
-			cols[i][row_num-1] = 'q';
+			cols[i][row_num-1] = 'X';
 			cols[i][row_num] = '\0';
 		}
 	}
@@ -95,11 +100,54 @@ void encrypt() {
 	fclose(ciphertext);
 }
 
+void decrypt() {
+	count = 0;
+	int i;
+	// create strings to store each column's data.
+	char* cols[col];
+	for (i = 0; i < col; i++) {
+		cols[i] = (char*)malloc(sizeof(char) * MAX_ROW);
+	}
+	// open file.
+	ciphertext = fopen("cipher.txt", "r");
+	result = fopen("result.txt", "w");
+	if (result == NULL || ciphertext == NULL) {
+		printf("Can't open file!!!\n");
+		return;
+	}
+
+	int size;
+	i = 0;
+	while (size = fread(cols[i], 1, row_num, ciphertext)) {
+		cols[i][row_num] = '\0';
+		//printf("%d: %s\n", i, cols[i]);
+		i++;
+	}
+
+	for (i = 0; i < col; i++) {
+		order[permutation[i]-1] = i;
+	}
+
+	// 
+	int row_num = strlen(cols[0]);
+	int j;
+	for (i = 0; i < row_num; i++) {
+		for (j = 0; j < col; j++) {
+			if (cols[order[j]][i] != 'X')
+				fputc(cols[order[j]][i], result);
+		}
+	}
+
+	fclose(ciphertext);
+	fclose(result);
+}
+
 int main () {
 	printf("Input key (lower case):\n");
 	gets(key);
 
 	get_permutation();
 	encrypt();
+	decrypt();
 	return 0;
 }
